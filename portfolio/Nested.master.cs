2520935c839d621c9642667bcdcffe1000a258ed
preet19 <Dilpreet.Singh2@MyGeorgian.ca>
2016-06-01 22:36:01 -0400

    added a working contact form

--- portfolio/Nested.master.cs
+++ portfolio/Nested.master.cs
@@ -16,7 +16,8 @@ namespace portfolio
 
         protected void SendButton_Click(object sender, EventArgs e)
         {
-            Response.Redirect("Default.aspx");
+            //Response.Redirect("Default.aspx");
+            SendMail();
         }
 
         protected void CancelButton_Click(object sender, EventArgs e)
@@ -27,5 +28,38 @@ namespace portfolio
             ContactNumberTextBox.Text = "";
             MessageTextBox.Text = "";
         }
+
+
+
+
+        protected void SendMail()
+        {
+            // Gmail Address from where you send the mail
+            var fromAddress = "whoishostingwebsite@gmail.com";
+            // any address where the email will be sending
+            var toAddress = EmailTextBox.Text.ToString();
+            //Password of your gmail address
+            const string fromPassword = "shehraj007";
+            // Passing the values and make a email formate to display
+            string subject = MessageTextBox.Text.ToString();
+            string body = "From: " + FirstNameTextBox.Text + "\n";
+            body += "Email: " + EmailTextBox.Text + "\n";
+            body += "Subject: " + MessageTextBox.Text + "\n";
+            body += "Question: \n" + MessageTextBox.Text + "\n";
+            // smtp settings
+            var smtp = new System.Net.Mail.SmtpClient();
+            {
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                smtp.Credentials = new System.Net.NetworkCredential(fromAddress, fromPassword);
+                smtp.Timeout = 20000;
+            }
+            // Passing values to smtp object
+            smtp.Send(fromAddress, toAddress, subject, body);
+        }
+
+        
     }
 }
